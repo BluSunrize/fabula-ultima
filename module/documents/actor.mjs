e0c5cacb680ac559df312dfc804aa6c1a6f93bcd
BluSunrize <danny.rad10@yahoo.de>
2023-06-16 09:55:12 +0000

    Fix health for character tokens being broken all the time

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -43,14 +43,9 @@ export class FabulaUltimaActor extends Actor {
   _prepareCharacterData(actorData) {
     if (this.type !== 'character') return;
 
-    // Make modifications to data here. For example:
-    //const data = actorData.data;
-
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    //for (let [key, ability] of Object.entries(data.abilities)) {
-    // Calculate the modifier using d20 rules.
-    //ability.mod = Math.floor((ability.value - 10) / 2);
-    //}
+    actorData.health.max = this.getMaxHealthPoints();
+    actorData.mind.max = this.getMaxMindPoints();
+    actorData.inventory.max = this.getMaxInventoryPoints();
   }
 
   /**
